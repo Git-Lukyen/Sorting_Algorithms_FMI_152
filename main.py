035f3076d0eadacaf1d15b6844dc3163c1679b93
lucan8 <136254363+lucan8@users.noreply.github.com>
2024-03-27 11:47:55 +0200

    Added benchmark function

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from utils.tests import test_generator, test_handler
 from utils.tests.test_generator import TestConfig
 from utils.algorithms.algorithms import Complexity
 import sys
-import time
-
+from time import time, sleep
+import multiprocessing
 
 def main():
     # Example generating tests
@@ -16,99 +16,72 @@ def main():
     test_generator.generate_test("easy_verification_test", [
         TestConfig(10, 100),
         TestConfig(100, 1000)])
-
-    # Example reading tests
-    tests = test_handler.read_file(
-        "tests/random_generated/easy_verification_test")
-
-    # Example using a sort algo
-    default_algo = algorithms.DefaultSort(
-        "Python Default Sort", Complexity.N_LOG2_N, True)
-
-    target_test = tests[0]
-    sorted_arr = default_algo.sort(array=target_test.array,
-                                   array_size=target_test.length,
-                                   max_value=target_test.max_value)
-
-    test_handler.verify_test(
-        sorted_arr, target_test.length, target_test.max_value)
-
-    # Quicksort test
-    quicksort = algorithms.QuickSort(
-        "Quick sort algorithm", Complexity.N_LOG2_N, False, sys.maxsize, 30_000_000)
-
-    target_test = tests[0]
-    sorted_arr = quicksort.sort(array=target_test.array,
-                                array_size=target_test.length,
-                                max_value=target_test.max_value)
-
-    test_handler.verify_test(
-        sorted_arr, target_test.length, target_test.max_value)
-
-    # Heapsort test
-    heapsort = algorithms.HeapSort(
-        "Heap sort algorithm", Complexity.N_LOG2_N, True, sys.maxsize, 30_000_000)
-
-    target_test = tests[0]
-    sorted_arr = heapsort.sort(array=target_test.array,
-                               array_size=target_test.length,
-                               max_value=target_test.max_value)
-
-    test_handler.verify_test(
-        sorted_arr, target_test.length, target_test.max_value)
     
     test_generator.generate_test("radix_verification_test", [
-        TestConfig(10, 100),
-        TestConfig(100, 1000),
-        TestConfig(1_000, 1_000),
-        TestConfig(1_000, 1_000_000_000_000),
+        # TestConfig(10, 100),
+        # TestConfig(100, 1000),
+        # TestConfig(1_000, 1_000),
+        # TestConfig(1_000, 1_000_000_000_000),
         TestConfig(1_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000),
-        TestConfig(10_000, 10_000),
-        TestConfig(10_000, 1_000_000),
+        # TestConfig(10_000, 10_000),
+        # TestConfig(10_000, 1_000_000),
         TestConfig(10_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000),
-        TestConfig(100_000, 100_000),
+        # TestConfig(100_000, 100_000),
         TestConfig(100_000, 1_000_000_000),
-        TestConfig(100_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000),
-        TestConfig(1_000_000, 1000),
-        TestConfig(1_000_000, 100_000),
+        TestConfig(200_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000),
+        # TestConfig(500_000, 1_000_000_000),
+        # TestConfig(1_000_000, 1_000),
         TestConfig(1_000_000, 1_000_000),
         TestConfig(1_000_000, 1_000_000_000),
-        #TestConfig(1_000_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000)
+        #TestConfig(10_000_000, 10_000),
+        #TestConfig(10_000_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000)
         ])
 
-    #RadixSort test
-    r_tests = test_handler.read_file("tests/random_generated/radix_verification_test")
-    
-    radix_algo = algorithms.RadixSort("Radix", Complexity.N_PLUS_MAX, False, 1_000_000, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000)
-    print("Radix tests:")
-    for target_test in r_tests:
-        start = time.time()
-        sorted_arr = radix_algo.sort(array=target_test.array,
-                                        array_size=target_test.length,
-                                        max_value=target_test.max_value)
-        try:
-            test_handler.verify_test(sorted_arr, target_test.length, target_test.max_value)
-            print("passed", time.time() - start)
-        except:
-            print("Not passed")
-    
-    #ShellSort test
-    s_tests = test_handler.read_file("tests/random_generated/verification_test")
-    
-    shell_algo = algorithms.ShellSort("ShellSort", Complexity.N_LOG2_N, True, 1_000_000, 1_000_000)
-    print("ShelSort tests:")
-    for target_test in s_tests:
-        start = time.time()
-        sorted_arr = shell_algo.sort(array=target_test.array,
-                                        array_size=target_test.length,
-                                        max_value=target_test.max_value)
-        try:
-            test_handler.verify_test(sorted_arr, target_test.length, target_test.max_value)
-            print("passed", time.time() - start)
-        except:
-            print("Not passed")
-            
+
+    test_path = "tests/random_generated/verification_test"
+
+    # Python sort
+    default_algo = algorithms.DefaultSort(
+        "Python Default Sort", Complexity.N_LOG2_N, True)
+    benchmark(default_algo, test_path)
+
+    #Quicksort test
+    quicksort = algorithms.QuickSort(
+        "Quick sort algorithm", Complexity.N_LOG2_N, False, sys.maxsize, 30_000_000)
+    benchmark(quicksort, test_path)
+
+   
+    #Heapsort test
+    heapsort = algorithms.HeapSort(
+        "Heap sort algorithm", Complexity.N_LOG2_N, True, sys.maxsize, 30_000_000)
+    benchmark(heapsort, test_path)
+
+    #Radix test(Max value incorrectly set)
+    test_path = "tests/random_generated/radix_verification_test"
+
+    radix_alg = algorithms.RadixSort("Radix", Complexity.N_PLUS_MAX, False, 10, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000, 1_000_000)
+    benchmark(radix_alg, test_path)
+
+    #Shell test(Max val can be anything really)
+    shell_alg = algorithms.ShellSort("ShellSort", Complexity.N_LOG2_N, False, 1_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000_000, 200_00)
+    benchmark(shell_alg, test_path)
 
 
 if __name__ == "__main__":
     main()
+
+
+
+def benchmark(alg : algorithms.SortingAlgorithm, filePath : str):
+    tests = test_handler.read_file(filePath)
+
+    print(alg.name, "tests: ")
+    for test in tests:
+        sorted_arr, duration = alg.sortTimed(array = test.array, 
+                                             array_size=test.length, 
+                                             max_value=test.max_value)
+        try:
+            test_handler.verify_test(sorted_arr, test.length, test.max_value)
+            print("Passed:", duration, "s")
+        except AssertionError:
+            print("Failed")
